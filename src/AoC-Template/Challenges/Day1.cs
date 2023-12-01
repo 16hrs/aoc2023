a853f721220c4c818e671b2edee2376382e50388
vos <72436899+16hrs@users.noreply.github.com>
2023-12-01 15:24:42 +0100

    day1 part1

---
+++ src/AoC-Template/Challenges/Day1.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCode2022.Running;
+using System.Collections.Immutable;
+using System.Globalization;
+
+namespace AdventOfCode2022.Challenges;
+
+public class Day1 : BaseChallenge
+{
+    private readonly string[] _input = LoadInput(File.ReadAllLines, nameof(Day1));
+
+    public override string SolvePartOne() =>
+        _input
+            .Sum(
+                x =>
+                    int.Parse(
+                        $"{x.First(char.IsNumber).ToString()}{x.Last(char.IsNumber).ToString()}"
+                    )
+            )
+            .ToString();
+
+    public override string SolvePartTwo()
+    {
+        var numbers = new HashSet<string>
+        {
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten"
+        };
+    }
+}
